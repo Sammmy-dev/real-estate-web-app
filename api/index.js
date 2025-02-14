f7e7e25e27c52d2230b3efae11232ffc46ca23b9
Samuel <ajiboyeadebayo901@gmail.com>
2025-02-14 16:33:28 +0100

    Create and run server

---
+++ api/index.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const app = express();
+//! To use "import" key word, add '"type":"module" to package.json file
+
+app.listen(3000,()=>{console.log("Server is running on port 3000!!!")});
+
