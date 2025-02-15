5cae2f0f7aab1d20944dbe1dfd448a74cd25ab7c
Samuel <ajiboyeadebayo901@gmail.com>
2025-02-15 06:11:36 +0100

    Created api test route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose= require("mongoose")
-const dotenv= require("dotenv")
+const dotenv= require("dotenv");
+const userRouter = require("./routes/user.route");
 dotenv.config();
 const app = express();
 //! To use "import" keyword, add '"type":"module" to package.json file
@@ -12,4 +13,6 @@ mongoose.connect(process.env.MONGOURL).then(
 })
 
 app.listen(3000,()=>{console.log("Server is running on port 3000!!!")});
+
+app.use("/api/user", userRouter)
 
